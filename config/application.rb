f53a7ea2ff5031767ba70f8b720f7ae221e7c67a
Jan Florjanczyk <jan.florjanczyk@gmail.com>
2025-09-28 18:44:00 -0700

    Load secret_key_base in application

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module TarymaCom
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Configure secret_key_base from credentials
+    config.secret_key_base = Rails.application.credentials.secret_key_base
   end
 end
